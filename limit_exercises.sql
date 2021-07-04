bba6989abab9a0cea83a102ae9d7d7c6cf70d536
Sarah Woods <sarahwoods@Sarahs-MacBook-Pro.local>
2021-07-03 21:43:48 -0500

    Made corrections and added more detailed comments.

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,24 +1,22 @@
-SELECT DISTINCT title FROM titles;
-
 USE employees;
 
+-- 2
 SELECT DISTINCT last_name
 FROM employees
 ORDER BY last_name DESC
 LIMIT 10;
--- 2
 
+-- 3 Cappello, Mandell, Schreiter, Kushner, Stroustrup
 SELECT last_name
 FROM employees
-WHERE hire_date LIKE '19%%' AND birth_date LIKE '19%%-12-25'
+WHERE hire_date LIKE '199%' AND birth_date LIKE '19%%-12-25'
 ORDER BY hire_date
 LIMIT 5;
--- 3 Dechter, Camrinopoulos, Radhakrishnan, Biros, Kemmerer
 
+-- 4 The relationship between offset and limit is that offset determines where to begin the count for the limit
 SELECT last_name
 FROM employees
 WHERE hire_date LIKE '19%%' AND birth_date LIKE '19%%-12-25'
 ORDER BY hire_date
 LIMIT 5 OFFSET 45;
--- The relationship between offset and limit is that offset determines where to begin the count for the limited
 
